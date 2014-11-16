69fc61321dfc47a879993be87614220f40ee4475
Oliver Faircliff <olls@ovi.com>
2014-11-16 13:28:07 -0500

    working for sentances

--- main.js
+++ main.js
@@ -1,28 +1,56 @@
 var http = require('http');
 var url = require('url');
+var async = require('async');
 
 var key = '2654c7e8967d5b240b860d9cb18f9b9e';
 
-var app = http.createServer(function (req, res1) {
-  console.log(req.url)
+function sortByKey(array, key) {
+    return array.sort(function(a, b) { 
+        var x = a[key];
+        var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+var app = http.createServer(function (req, res) {
   var url_parts = url.parse(req.url, true);
-  var search = url_parts.query.search
-  search = search.split("+")
-  var songs = []
-  var c = 0
-  for(var i = 0; i<search.length; i++){
-    
-    get_song(res1,search[i], function (song) {
-      songs.push(song)
-      c++
-    })
+  var search = url_parts.query.search;
+
+  if(!search) {
+    res.end('error');
+    return;
   }
 
-  while (c < songs.length) {}
+  var words = search.split(" ")
+  var i = 0;
+  words.forEach(function (word) {
+    words[i++] = {i: i, word: word};
+  });
+  console.log(words)
 
-  req.end(songs)
+  var songs = [];
 
-  
+  async.each(words, function (word, cb) {
+    get_song(res, word.word, function (song) {
+      songs.push({song: song, i: word.i});
+      cb();
+    })
+  }, function (err) {
+    if (err) {
+      console.log('err');
+      return;
+    }
+
+    console.log(songs)
+
+    var out = '';
+    songs = sortByKey(songs, 'i');
+    songs.forEach(function(song){
+      out += song.song + '\n';
+    });
+    res.end(out);
+  });
+
 });
 
 function get_song(result, search, cb) {
