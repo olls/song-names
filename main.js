f1af69bb48679b0278f5e9c608af5a9309aa4f24
KevTee123 <KevTee123@hotmail.co.uk>
2014-11-16 17:12:26 +0000

    Added sentence parsing

--- main.js
+++ main.js
@@ -6,18 +6,36 @@ var key = '2654c7e8967d5b240b860d9cb18f9b9e';
 var app = http.createServer(function (req, res1) {
   console.log(req.url)
   var url_parts = url.parse(req.url, true);
-  var search = url_parts.query.search;
+  var search = url_parts.query.search
+  search = search.split("+")
+  var songs = []
+  var c = 0
+  for(var i = 0; i<search.length; i++){
+    
+    get_song(res1,search[i], function (song) {
+      songs.push(song)
+      c++
+    })
+  }
+
+  while (c < songs.length) {}
+
+  req.end(songs)
+
+  
+});
+
+function get_song(result, search, cb) {
   var buffer = ''
   var tiny = http.get("http://tinysong.com/b/"+search+"?format=json&key="+key,function(res){
     res.on('data',function(data){
       buffer+=data
     });  
     res.on('end',function(){
-     // console.log(buffer) 
-      res1.end(JSON.parse(buffer).SongName)
+      cb(JSON.parse(buffer).SongName)
     });
-  });  
-});
+  });
+}
 
 app.listen(6002);
 
