d759a583bf596b725e55e72bdbb2d1dcad548d2e
Oliver Faircliff <olls@ovi.com>
2014-11-16 10:23:40 -0500

    added key

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var http = require('http');
 
+var key = '2654c7e8967d5b240b860d9cb18f9b9e';
+
 var app = http.createServer(function (req, res) {
   res.end('Hello World');
 });
 
-app.listen(6002)
+app.listen(6002);
+
